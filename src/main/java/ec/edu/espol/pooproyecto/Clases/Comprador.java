50970d1c39e0e6db67ae6dfd0d9c5e0dcdd920e3
Cykes <>
2023-07-09 01:30:38 -0500

    Añadi los hashcode, sigo sin saber  como funciona pero funciona. Ahora si hice bien  el git

--- src/main/java/ec/edu/espol/pooproyecto/Clases/Comprador.java
+++ src/main/java/ec/edu/espol/pooproyecto/Clases/Comprador.java
@@ -4,6 +4,8 @@
  */
 package ec.edu.espol.pooproyecto.Clases;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 /**
@@ -16,5 +18,19 @@ public class Comprador extends Usuario{
     public Comprador(String nommbre, String apellido, String organizacion, String correo, String clave) {
         super(nommbre, apellido, organizacion, correo, clave);
     }
-//hola
+   public static String generarHash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
 }
--- src/main/java/ec/edu/espol/pooproyecto/Clases/Vendedor.java
+++ src/main/java/ec/edu/espol/pooproyecto/Clases/Vendedor.java
@@ -4,6 +4,8 @@
  */
 package ec.edu.espol.pooproyecto.Clases;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -27,6 +29,22 @@ public class Vendedor extends Usuario {
         }
         
         return false;
+    }
+    
+       public static String generarHash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
     }
     
     public void registrarNuevoVendedor(Scanner sc){
